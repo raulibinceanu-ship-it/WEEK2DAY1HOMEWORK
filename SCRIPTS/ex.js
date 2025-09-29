95f0f4c90987b160a1e893a8d38987f32247c935
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2025-09-29 17:15:57 +0200

    Update ex.js

---
+++ SCRIPTS/ex.js
@@ -0,0 +1,37 @@
+/* ESERCIZIO 1
+
+// - String: sono parole o frasi, racchiuse tra virgolette. Es: "ciao"
+// - Number: sono numeri, con cui possiamo fare calcoli. Es: 10 o 3.5
+// - Boolean: sono come interruttori, che possono essere accesi (true) o spenti (false).
+// - Undefined: significa che una variabile esiste, ma non ha ancora un valore.
+// - Null: significa "niente", una scatola vuota che sappiamo esserlo apposta.
+// - Object: sono "scatole" più grandi che contengono tanti dati insieme (come un pupazzo con accessori).
+// - Array: sono liste ordinate di valori, come una fila di oggetti uno accanto all’altro.
+*/
+
+// ESERCIZIO 2
+
+let myName = "Marco";
+
+// ESERCIZIO 3
+
+let somma = 12 + 20;
+
+// ESERCIZIO 4
+
+let x = 12;
+
+//ESERCIZIO 5
+
+myName = "Rossi";
+
+//ESERCIZIO 6
+
+let sottrazione = 4 - x; // risultato: -8
+
+// ESERCIZIO 7
+
+let name1 = "john";
+let name2 = "John";
+
+let sonoDiversi = name1 !== name2; // true
